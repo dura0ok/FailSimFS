6c8732e547cee2d061992c55d6381df2838e36c2
durachok <fenixrnd@mail.ru>
2024-03-08 23:31:45 +0700

    current state

--- config/json_config_loader.py
+++ config/json_config_loader.py
@@ -1,15 +1,29 @@
 import json
+import re
 
+from typing import Optional, Dict, Any
 from .config_loader import ConfigLoader
 
 
 class JsonConfigLoader(ConfigLoader):
-    def __init__(self, config_file_name):
-        self.config_file_name = config_file_name
+    def __init__(self, config_file_name: str) -> None:
+        self.config_file_name: str = config_file_name
 
-    def load_config(self):
+    def load_config(self) -> Dict[str, Any]:
         with open(self.config_file_name, 'r') as f:
             return json.load(f)
 
-    def get_replacement(self, path, syscall_name):
-        return self.load_config().get(path, {}).get(syscall_name)
+    def find_matching_key(self, path: str) -> Optional[str]:
+        config_data: Dict[str, Any] = self.load_config()
+        for key in config_data:
+            if re.match(path, key):
+                return key
+        return None
+
+    def get_replacement(self, path: str, syscall_name: str) -> Optional[Dict[str, str]]:
+        matching_key: Optional[str] = self.find_matching_key(path)
+
+        if matching_key is not None:
+            return self.load_config()[matching_key].get(syscall_name)
+
+        return None
--- custom_module.py
+++ custom_module.py
@@ -1,10 +1,12 @@
 import os
+from random import randint
 
+from fuse import FuseOSError
 from print_color import print
 
 
 def get_attr(path, fh=None):
-    print(f"DEBUG: Custom getattr for {path} {fh} with logging :)", color='purple')
+    # print(f"DEBUG: Custom getattr for {path} {fh} with logging :)", color='purple')
     try:
         st = os.lstat(path)
         return dict((key, getattr(st, key)) for key in
@@ -12,3 +14,16 @@ def get_attr(path, fh=None):
     except OSError as e:
         print(f"ERROR: {e}")
         return None
+
+
+def read(path, size, offset, fh=None):
+    print("DEBUG", color='green')
+    try:
+        with open(path, 'rb') as f:
+            data = f.read()
+            if randint(0, 1):
+                print(data)
+                return b''  # Return empty data as a no-op
+            return data[offset:offset + size]
+    except FileNotFoundError:
+        raise FuseOSError(2)
