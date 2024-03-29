a96c54b09f57b9e05db4088d6de6d95d0a0ac443
durachok <fenixrnd@mail.ru>
2024-03-29 17:59:52 +0700

    current state

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import argparse
 from fuse import FUSE
-
 from configurable_fs import ConfigurableFS
 
-if __name__ == '__main__':
-    mount_point = 'mnt_point'
-    root_path = 'data'
-    config_file = 'example-config.json'
 
+def main(mount_point, root_path, config_file):
     fs = ConfigurableFS(root_path, config_file)
     fuse = FUSE(fs, mount_point, foreground=True)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Mount a configurable file system.')
+    parser.add_argument('mount_point', type=str, help='Path to the mount point')
+    parser.add_argument('root_path', type=str, help='Root path of the file system')
+    parser.add_argument('config_file', type=str, help='Path to the configuration file')
+
+    args = parser.parse_args()
+    main(args.mount_point, args.root_path, args.config_file)
